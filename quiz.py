1337ec81a193a2964bbc3cff6c6bb1c7b023b6c2
EthanGardner18 <ethangrdnr@gmail.com>
2024-01-29 16:22:20 -0600

    added

--- quiz.py
+++ quiz.py
@@ -1,7 +1,7 @@
 import random
 print("LETS PLAY THE QUIZ GAME!")
 
-guess = (int)(input("Guess a number 1 though 20: "))
+guess = (int)(input("Guess a number 1 though 30: "))
 
 if (guess == random.randint(1,10)):
     print("You guessed Correctly!")
