65aad0a2143d1afb978ae8c55ce3fda578d44ed4
EthanGardner18 <ethangrdnr@gmail.com>
2024-01-29 16:52:49 -0600

    sdklag

--- quiz.py
+++ quiz.py
@@ -2,7 +2,6 @@ import random
 print("LETS PLAY THE QUIZ GAME!")
 
 guess = (int)(input("Guess a number 1 though 1000: "))
-guess = (int)(input("Guess a number 1 though 200: "))
 
 if (guess == random.randint(1,10)):
     print("You guessed Correctly!")
