0593d2ef64a10eb2de447202d6b231f5270552b0
unknown <supadhy@siue.edu>
2024-01-29 17:58:08 -0600

    quiz

--- quiz.py
+++ quiz.py
@@ -1,7 +1,7 @@
 import random
 print("LETS PLAY THE QUIZ GAME!")
 
-guess = (int)(input("Guess a number 1 though 1000: "))
+guess = (int)(input("Guess a number 1 though 500: "))
 
 if (guess == random.randint(1,10)):
     print("You guessed Correctly!")
